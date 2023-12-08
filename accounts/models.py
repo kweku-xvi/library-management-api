40cb789cced46be3e3c7804436926a887d271451
kweku-xvi <nksarps@gmail.com>
2023-12-08 21:11:47 +0000

    added user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,60 @@
+import uuid
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+
+class MyUserManager(BaseUserManager):
+    def create_user(self,email,password:None,**extra_fields):
+        if not email:
+            raise ValueError(_("The email is required"))
+        email = self.normalize_email(email=email)
+        user = self.model(email=email,**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self,email,password=None,**extra_fields):
+        extra_fields.setdefault("is_staff",True)
+        extra_fields.setdefault("is_superuser",True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("SuperUser must have is_staff=True"))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("SuperUser must have is_superuser=True"))
+        
+        return self.create_user(email,password,**extra_fields)
+
+
+class User(AbstractUser):
+    ROLE_CHOICES = [
+        ('Member', 'Member'),
+        ('Librarian', 'Librarian')
+    ]
+
+    id = models.CharField(unique=True, primary_key=True, max_length=8)
+    first_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=50, choices=ROLE_CHOICES, default='Member')
+    address = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=50, unique=True)
+    date_joined = models.DateField(auto_now_add=True)
+    is_verified = models.BooleanField(default=False)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'phone_number']
+
+    def __str__(self):
+        return self.username
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.id = str(uuid.uuid4())[:8]
+        super().save(*args, **kwargs)
+
+    class Meta:
+        ordering = ('-date_joined',)
