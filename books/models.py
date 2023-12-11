45e68db5af864e7055997c54fec686350301cbec
kweku-xvi <nksarps@gmail.com>
2023-12-11 12:24:57 +0000

    add  books and checkout models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,36 @@
+import uuid
+from accounts.models import User
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Book(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4)
+    isbn = models.CharField(max_length=13)
+    title = models.CharField(max_length=255)
+    cover_image = models.ImageField(blank=True, null=True)
+    description = models.TextField()
+    authors = models.CharField(max_length=255)
+    genre = models.CharField(max_length=255)
+    date_published =  models.DateField(default=timezone.now)
+    publisher = models.CharField(max_length=255)
+    number_of_pages = models.PositiveIntegerField(default=0)
+    language = models.CharField(max_length=255)
+    checked_out = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('-created_at',)
+
+
+class CheckoutBook(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    due_date = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.book.title}"
