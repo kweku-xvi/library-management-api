d99fc4d93b14a3b787ee09f3f0e6cde5d4a6588a
kweku-xvi <nksarps@gmail.com>
2023-12-12 11:41:04 +0000

    update checkoutbook model

--- books/models.py
+++ books/models.py
@@ -1,5 +1,6 @@
 import uuid
 from accounts.models import User
+from datetime import timedelta
 from django.db import models
 from django.utils import timezone
 
@@ -27,10 +28,15 @@ class Book(models.Model):
 
 
 class CheckoutBook(models.Model):
-    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4)
+    checkout_id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    due_date = models.DateField(default=timezone.now)
+    borrow_date = models.DateField(default=timezone.now)
+    due_date = models.DateField(default=(timezone.now() + timedelta(days=21)))
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.user.username} - {self.book.title}"
+
+    class Meta:
+        ordering = ('-created_at',)
