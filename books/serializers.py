e7fd87d9d74c49a1d6d810ee62c36cc8549c17aa
kweku-xvi <nksarps@gmail.com>
2023-12-12 11:41:38 +0000

    remove checkoutbookserializer

--- books/serializers.py
+++ books/serializers.py
@@ -7,17 +7,3 @@ class BookSerializer(serializers.ModelSerializer):
         model = Book
         fields = ['isbn', 'title', 'cover_image', 'description', 'authors', 'genre', 'date_published', 'publisher', 'number_of_pages', 'language', 'checked_out']
 
-
-# class CheckoutBookSerializer(serializers.ModelSerializer):
-#     book = serializers.SerializerMethodField()
-#     user = serializers.SerializerMethodField()
-
-#     class Meta:
-#         model = CheckoutBook
-#         fields = ['id', 'book', 'user', 'due_date']
-
-#     def get_book(self, obj):
-#         return obj.book.title if obj.book else None
-
-#     def get_user(self, obj):
-#         return obj.user.username if obj.user else None
