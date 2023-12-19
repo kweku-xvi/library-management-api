50194a00f96f1cd62745656706faa29aee74b3b1
kweku-xvi <nksarps@gmail.com>
2023-12-19 02:06:16 +0000

    add reserve books model

--- books/models.py
+++ books/models.py
@@ -6,6 +6,11 @@ from django.utils import timezone
 
 
 class Book(models.Model):
+    AVAILABILITY_CHOICES = [
+        ('Available', 'Available'),
+        ('Unavailable', 'Unavailable')
+    ]
+
     id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4)
     isbn = models.CharField(max_length=13)
     title = models.CharField(max_length=255)
@@ -17,7 +22,8 @@ class Book(models.Model):
     publisher = models.CharField(max_length=255)
     number_of_pages = models.PositiveIntegerField(default=0)
     language = models.CharField(max_length=255)
-    checked_out = models.BooleanField(default=False)
+    available = models.CharField(max_length=50, choices=AVAILABILITY_CHOICES, default='Available')
+    shelf_location = models.CharField(max_length=255, default='N/A')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -37,6 +43,19 @@ class CheckoutBook(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.book.title}"
+
+    class Meta:
+        ordering = ('-created_at',)
+
+
+class ReserveBook(models.Model):
+    reservation_id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reserves')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} reserved "{self.book.title}"'
 
     class Meta:
         ordering = ('-created_at',)
